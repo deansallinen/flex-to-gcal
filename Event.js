c2d1178997e342495c1613a5be3899edc2a688ad
Dean Sallinen <deza604@gmail.com>
2018-09-12 11:29:01 -0700

    completed scraper functionality to remote mongodb

--- Event.js
+++ Event.js
@@ -1,27 +1,43 @@
-class Event {
-  constructor(event) {
-    this.elementId = event.elementId;
-    this.startDate = new Date(event.startDate);
-    this.endDate = new Date(event.endDate);
-    this.status = event.status;
-    this.manager = event.personResponsibleName;
-    this.displayText = event.displayText;
-    this.typeId = event.typeId;
-    this.typeName = event.typeName;
-    this.locationName = event.locationName;
-  }
+const db = require('./db.js');
+// const Cat = db.model('Cat', { name: String });
+const Event = db.model('Event', {
+  elementId: String,
+  startDate: Date,
+  endDate: Date,
+  status: String,
+  personResponsibleName: { type: String, alias: 'manager' },
+  displayText: String,
+  typeId: String,
+  typeName: String,
+  locationName: String,
+  loadInDate: String,
+  loadOutDate: String,
+  lastScraped: Date,
+});
+// class Event {
+//   constructor(event) {
+//     this.elementId = event.elementId;
+//     this.startDate = new Date(event.startDate);
+//     this.endDate = new Date(event.endDate);
+//     this.status = event.status;
+//     this.manager = event.personResponsibleName;
+//     this.displayText = event.displayText;
+//     this.typeId = event.typeId;
+//     this.typeName = event.typeName;
+//     this.locationName = event.locationName;
+//   }
 
-  get id() {
-    return this.elementId.replace(/-/g, '');
-  }
+//   get id() {
+//     return this.elementId.replace(/-/g, '');
+//   }
 
-  get title() {
-    return this.displayText.slice(10);
-  }
+//   get title() {
+//     return this.displayText.slice(10);
+//   }
 
-  get number() {
-    return this.displayText.slice(0, 7);
-  }
-}
+//   get number() {
+//     return this.displayText.slice(0, 7);
+//   }
+// }
 
 module.exports = Event;
--- main.js
+++ main.js
@@ -1,37 +1,26 @@
-const diskdb = require('diskdb');
 const { getFlexCal, getFlexDetails } = require('./getFlex');
 const Event = require('./Event');
 
-const db = diskdb.connect('./', ['data']);
+const startDate = new Date();
+const endDate = new Date();
 
-const testId = 'f3c10bf0-0dca-11e8-9702-0030489e8f64';
+const mergeDetails = (event, details) => ({ ...event, ...details, lastScraped: new Date() });
 
-
-const getSchedule = async (start, end) => {
-  const flexCal = await getFlexCal(start, end);
-  const schedule = await flexCal.map(each => new Event(each));
-  return schedule;
-  //   return detailedSchedule;
+const upsertEvent = (event) => {
+  Event.findOneAndUpdate({ elementId: event.elementId }, event, { upsert: true });
 };
 
-const saveSchedule = async (scheduleInput) => {
-  const schedule = await scheduleInput;
-  return schedule.map(event => db.data.update({ elementId: event.elementId }, event, { upsert: true }));
-};
+const main = () => getFlexCal(startDate, endDate)
+  .then(fcal => fcal.map(event => getFlexDetails(event.elementId)
+    .then(details => mergeDetails(event, details))
+    .then(readyEvent => upsertEvent(readyEvent))
+    .catch((err) => { throw err; })))
+  .catch((err) => { throw err; });
 
-const getEventDetails = async (schedule) => {
-  schedule.map(event => console.log(event.elementId));
-//   const detailedEvent = Object.assign(getEventDetails(event.elementId), event);
-//   return detailedEvent;
-};
-
-const getOneEvent = id => db.data.findOne({ elementId: id });
-const eventExists = id => !!(getOneEvent(id));
-
-const main = async () => {
-  const schedule = await getSchedule(new Date(), new Date());
-  const detailedSchedule = await getEventDetails(schedule);
-  const saved = await saveSchedule(schedule);
-  const details = await addEventDetails();
-};
 main();
+
+module.exports = {
+  main,
+  mergeDetails,
+  upsertEvent,
+};
